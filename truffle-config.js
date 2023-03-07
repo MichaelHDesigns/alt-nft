42f2f8b3320946bb160413fbecc1f621ed5026b8
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-06 21:20:13 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,39 +1,96 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const keys = require("./keys.json");
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
+ *
+ *   > > $ npm install truffle-hdwallet-provider@web3-one
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+const HDWallet = require("@truffle/hdwallet-provider");
+const developmentMnemonic =
+  "favorite myth child gain obvious push butter museum chunk blame evolve floor";
+
+// const infuraKey = "fj4jll3k.....";
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  contracts_build_directory: "./public/contracts",
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
   networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
     development: {
-     host: "127.0.0.1", 
-     port: 8545,
-     network_id: "2330",
-       provider: () => 
-        new HDWalletProvider(
-          keys.PRIVATE_KEY,
-          keys.INFURA_ROPSTEN_URL
-        ),
+      provider: () =>
+        new HDWallet(developmentMnemonic, "https://rpc0.altcoinchain.org/rpc"),
+        network_id: "2330" // Any network (default: none)
     },
-    ropsten: {
-      provider: () => 
-        new HDWalletProvider(
-          keys.PRIVATE_KEY,
-          keys.INFURA_ROPSTEN_URL
-        ),
-      network_id: 2330,
-      gas: 30000000,
-      gasPrice: 20000000000,
-      confirmations: 2,
-      timeoutBlocks: 200,
-    }
+    geth: {
+      provider: () =>
+        new HDWallet(developmentMnemonic, "http://127.0.0.1:8545"),
+      network_id: "*"
+    },
+
+    // Another network with more advanced options...
+    advanced: {
+       provider: () =>
+        new HDWallet(developmentMnemonic, "https://rpc0.altcoinchain.org/rpc"),
+       port: 8545,             // Custom port
+       network_id: 2330,       // Custom network
+       gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+       gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+       from: 0x8dA3dA022d7a5224615c8F2E3fFdDc5B883B24A2,        // Account to send txs from (default: accounts[0])
+       websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    },
   },
 
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13",
+       version: "pragma",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+      //  evmVersion: "byzantium"
+      }
     }
-  },
+  }
 };
-
-
-//works
